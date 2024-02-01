15aee59f79dcc0e2ff5d9ff6a07ec8c1a26919ad
Victor Garcia <victorbenitogr@gmail.com>
2024-02-01 09:33:27 -0600

    Update data file and read multiple sheets

--- src/Euromonitor.R
+++ src/Euromonitor.R
@@ -16,6 +16,17 @@ library(openxlsx) # Reading/writing/editing excel files
 
 # *** FILE *** ----------------------------------------------------------------
 
-getwd() # Change Working Directory with setwd("/path/to/DataCleaner") if needed
+# Specify the file path
+file_path <- "./data/market_data.xlsx"
 
-dataset <- read.xlsx("2022 PowerPoint Test - Market Data.xlsx")
+# Read each sheet into a separate data frame
+mexico_market_size <- read.xlsx(file_path, sheet = "mexico_market_size")
+mexico_market_share <- read.xlsx(file_path, sheet = "mexico_market_share")
+mexico_channel_breakdown <- read.xlsx(file_path,
+  sheet = "mexico_channel_breakdown"
+)
+usa_market_size <- read.xlsx(file_path, sheet = "usa_market_size")
+usa_market_share <- read.xlsx(file_path, sheet = "usa_market_share")
+usa_channel_breakdown <- read.xlsx(file_path, sheet = "usa_channel_breakdown")
+category_definitions <- read.xlsx(file_path, sheet = "category_definitions")
+channel_definitions <- read.xlsx(file_path, sheet = "channel_definitions")
